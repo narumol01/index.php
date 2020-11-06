8ae7e3539217868ff2f2f8e8c5577427c0dcd9bc
narumol01 <73689289+narumol01@users.noreply.github.com>
2020-11-06 18:41:18 +0700

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<? php
+echo '<html>';
+echo '<head>';
+echo '<title>';
+echo '<h1>Narumol Thongchue ANGPAO</h1>';
+echo '</title>';
+echo '</head>';
+echo '<body>';
+echo '<img src="https://i.redd.it/1procftja2s21.jpg">';
+echo '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRYGDPRSDJtE6_7zExoal-ECCi5MmhpfOxKNQ&usqp=CAU">';
+echo '<br>';
+echo '<a href="https://twitter.com/">Link --> </a>';
+echo '<img src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png">';
+echo '</br>';
+echo '<br>';
+echo '<p>ตำรวจจับผู้ร้ายได้คนหนึ่ง กำลังจะพาไปโรงพัก</p>';
+echo '<p>แต่เผอิญตำรวจอยากสูบบุหรี่ ผู้ร้ายจึงบอกว่า</p>';
+echo '</br>';
+echo '<ul>';
+echo '<li>ผู้ร้าย: เดี๋ยวผมไปซื้อให้ครับ</li>';
+echo '<li>ตำรวจ: แกคิดว่าฉันโง่รึไง ถ้าแกไปซื้อแกก็หนีสิ แกรออยู่ตรงนี้แหละเดี๋ยวฉันไปซื้อเอง</li>';
+echo '</ul>';
+echo '</body>';
+echo '</html>';
+?>
